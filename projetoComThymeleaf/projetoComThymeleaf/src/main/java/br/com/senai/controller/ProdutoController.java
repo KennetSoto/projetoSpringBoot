9b97d3bd6baa876a640cdc0b5cac8de192f0a867
kennet <kennet.soto@hotmail.com>
2023-08-11 21:48:43 -0300

    terceiro comit

--- projetoComThymeleaf/projetoComThymeleaf/src/main/java/br/com/senai/controller/ProdutoController.java
+++ projetoComThymeleaf/projetoComThymeleaf/src/main/java/br/com/senai/controller/ProdutoController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import br.com.senai.model.Produto;
@@ -45,6 +46,39 @@ public class ProdutoController {
 			return "adicionar_produto";
 		}
 		produtoRepository.save(produto);
+		return "redirect:/produto";
+	}
+	
+	@GetMapping("/editar/{id}")
+	public String paginaAtualizarProduto(
+			@PathVariable("id") long id, Model model) {
+		Produto produto = produtoRepository.findById(id)
+				.orElseThrow(() -> 
+				new IllegalArgumentException("Identificador do produto é inválido" + id));
+		
+		model.addAttribute("produto", produto);
+		return "editar_produto";
+	}
+	
+	@PostMapping("/atualizar/{id}")
+	public String atualizarProduto(@PathVariable("id") long id,
+			@Valid Produto produto, BindingResult result,
+			Model model) {
+		if(result.hasErrors()) {
+			produto.setId(id);
+			return "editar_produto";
+		}
+		produtoRepository.save(produto);
+		return "redirect:/produto";
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String deletarProduto(
+			@PathVariable("id") long id, Model model) {
+		Produto produto = produtoRepository.findById(id)
+				.orElseThrow(()-> 
+				new IllegalArgumentException("Identificador do produto inválido" + id));
+		produtoRepository.delete(produto);
 		return "redirect:/produto";
 	}
 	
